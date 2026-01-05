20d01eb100561c2bbac39555cadb4b04e607cd0b
cabral.ki <kikycabral@gmail.com>
2026-01-05 16:17:26 -0500

    added plot

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 # Hello World for PSI Numerical Methods 2026
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 2* np.pi, .1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+fig = plt.figure()
+plt.plot(x, y1, color='g',label='sin')
+plt.plot(x, y2, color='r', label='cos')
+plt.legend()
+plt.savefig("trig.png")
+
+print(np.exp(1.2))
+print("Hello PSI 2026!")
 
 def myexp(x, N=10):
     """
